9875229c5a3d99051abadc0440787b216e5e932f
junghanChoi <david.junghan91@gmail.com>
2019-04-07 17:07:50 +0900

    first commit

---
+++ src/main/naveenSeleniumJunghan/ZooplaMainPage.java
@@ -0,0 +1,7 @@
+package naveenSeleniumJunghan;
+
+public class ZooplaMainPage {
+	private String url = "https://www.zoopla.co.uk/";
+	
+	
+}
---
+++ src/test/naveenSeleniumJunghan/NaveenTest.java
@@ -0,0 +1,5 @@
+package naveenSeleniumJunghan;
+
+public class NaveenTest {
+
+}
