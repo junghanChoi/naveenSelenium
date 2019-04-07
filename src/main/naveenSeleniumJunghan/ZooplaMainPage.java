3a9244840ba7fd1571f6bc85f48baa17ff6b15e5
junghanChoi <david.junghan91@gmail.com>
2019-04-07 21:03:40 +0900

    naveen complete

--- src/main/naveenSeleniumJunghan/ZooplaMainPage.java
+++ src/main/naveenSeleniumJunghan/ZooplaMainPage.java
@@ -1,7 +1,58 @@
 package naveenSeleniumJunghan;
 
-public class ZooplaMainPage {
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.LoadableComponent;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ZooplaMainPage extends BasePage{
 	private String url = "https://www.zoopla.co.uk/";
+	
+	
+	@FindBy(css=".cookie-banner__button")
+	private WebElement cookieBanner;
+	
+	@FindBy(css="#search-input-location")
+	private WebElement forSaleInput;
+	
+	@FindBy(css="#search-submit")
+	private WebElement searchButton;
+	
+	
+	
+	public ZooplaMainPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SearchResultPage searchExactPlace(String location) {
+		forSaleInput.sendKeys(location);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[@class='ui-menu-item'][1]/a")));
+		WebElement exactLi = driver.findElement(By.xpath("//li[@class='ui-menu-item'][1]"));
+		exactLi.click();
+		searchButton.click();
+		return new SearchResultPage(driver);
+	}
+	
+	@Override
+	protected void isLoaded() throws Error {
+		// TODO Auto-generated method stub
+		assertTrue(driver.getTitle().contains("Zoopla"));
+	}
+
+	
+	@Override
+	protected void load() {
+		// TODO Auto-generated method stub
+		driver.get(url);
+		cookieBanner.click();
+	}
 	
 	
 }
--- src/test/naveenSeleniumJunghan/NaveenTest.java
+++ src/test/naveenSeleniumJunghan/NaveenTest.java
@@ -1,5 +1,41 @@
 package naveenSeleniumJunghan;
 
-public class NaveenTest {
+import static org.testng.Assert.assertTrue;
 
+import java.util.HashMap;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class NaveenTest {
+	WebDriver driver;
+	
+	@BeforeClass
+	@Parameters({"driverPath"})
+	public void setUp(String driverPath) {
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void scenario() {
+		ZooplaMainPage main = new ZooplaMainPage(driver);
+		main.get();
+		SearchResultPage result = main.searchExactPlace("London");
+		result.get();
+		result.printTrimedPrice();
+		DetailPage detail = result.moveToResultByIndex(4);
+		detail.get();
+		HashMap agentInfo1 = detail.getAgentInfo();
+		AgentPage agent = detail.moveToAgentPage();
+		agent.get();
+		HashMap agentInfo2 = agent.getAgentInfo();
+		
+		assertTrue(agentInfo1.equals(agentInfo2));
+		
+	}
 }
